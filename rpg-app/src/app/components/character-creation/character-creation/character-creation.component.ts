d6b5df3eed682735315f21b89328bdfedec00038
gierMann <thisisgie.21@gmail.com>
2020-06-17 17:14:01 +0700

    update: added events and functionalities for character creation

--- rpg-app/src/app/components/character-creation/character-creation/character-creation.component.ts
+++ rpg-app/src/app/components/character-creation/character-creation/character-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CharacterOptions} from '../../../models/character-options';
 
 @Component({
   selector: 'app-character-creation',
@@ -18,30 +19,51 @@ export class CharacterCreationComponent implements OnInit {
 
   characterComplete: boolean = false;
 
+  races = CharacterOptions.races;
+  classes = CharacterOptions.classes;
+  genders = CharacterOptions.genders;
+
+
   ngOnInit(): void {
   }
 
-  changeRace = () => {
+  checkCompleted() {
+    this.characterComplete = this.character.race !== "--Choose--"
+      && this.character.class ! == "--Choose--"
+      && this.character.gender
+      && this.character.name
 
+    console.log(this.characterComplete)
+  };
+
+  changeRace (race: string) {
+    this.character.race = race;
+    this.checkCompleted();
+  };
+
+  changeClass (newClass: string) {
+    this.character.class = newClass;
+    this.checkCompleted();
+  };
+
+  changeGender (gender: string) {
+
+    this.character.gender = gender;
+    this.checkCompleted();
+  };
+
+  changeName (){
+    this.checkCompleted();
+    this.characterComplete = true;
+  };
+
+  createCharacter(){
+    if(!this.characterComplete){
+      return;
+    }
+
+    console.log(this.character);
   }
-
-  changeClass = () => {
-    
-  }
-
-  changeName = () => {
-    
-  }
-
-  changeGender = () => {
-    
-  }
-
-  createCharacter = () => {
-    
-  }
-
-
 
 
 }
