55a3b18a529eb3a8a663540deb9049fbae774780
gierMann <thisisgie.21@gmail.com>
2020-06-17 13:20:57 +0700

    update: added app components

--- rpg-app/src/app/app.module.ts
+++ rpg-app/src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+import { CharacterCreationComponent } from './components/character-creation/character-creation/character-creation.component';
+import { FightComponent } from './components/fight/fight/fight.component';
+import { InventoryComponent } from './components/inventory/inventory/inventory.component';
+import { StartComponent } from './components/start/start/start.component';
+import { StoryComponent } from './components/story/story/story.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterCreationComponent,
+    FightComponent,
+    InventoryComponent,
+    StartComponent,
+    StoryComponent
   ],
   imports: [
     BrowserModule,
