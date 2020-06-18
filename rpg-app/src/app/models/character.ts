3f348849470667b072b7a9932c077a2f790a052a
gierMann <thisisgie.21@gmail.com>
2020-06-18 12:07:45 +0700

    update: base classes

--- rpg-app/src/app/models/character.ts
+++ rpg-app/src/app/models/character.ts
@@ -109,10 +109,6 @@ export class BaseCharacter {
 
 
 export class Monster extends BaseCharacter {
-    isTrapped : boolean = false;
-    poisonStacks : number = 0;
-    isStrongPoison : boolean = false;
-    hasTakenDamageThisTrun : boolean = false;
 
     constructor( name, health, skills, barriers : {attack: number , sneak: number, persuade: number}, minDamage, maxDamage, spriteUrl){
         super(name, health, skills);
@@ -120,6 +116,101 @@ export class Monster extends BaseCharacter {
         this.barriers = barriers;
         this.equippedWeapon = new Weapon(undefined, minDamage, maxDamage);
         this.spriteUrl = spriteUrl;
+    }
+
+    isTrapped : boolean = false;
+    poisonStacks : number = 0;
+    isStrongPoison : boolean = false;
+    hasTakenDamageThisTrun : boolean = false;
+
+}
+
+export class Hero extends BaseCharacter {
+    gender : string;
+    race: string; 
+    characterRole : string;
+    experience : number;
+    level: number;
+    availableSkillPoints: number;
+    hasTrapDefence : boolean;
+    hasDamagingTrap: boolean;
+    turnsUntilSpecialAvailableAgain : number;
+
+    constructor(name, gender, race, level, health, skills, weapon, armor){
+        super(name, health, skills);
+
+        this.gender = gender ;
+        this.race = race ;
+        this.experience = 0;
+        this.level = level;
+        this.equippedWeapon = weapon;
+        this.equippedNewArmor(armor);
+    }
+
+    // if you dont want to do anything in your data type, you can assign it as void
+    levelUp(): void{
+
+        // this is to reset the exp points when leveling up
+        this.experience -= ExperienceTolevel[this.level];
+        this.level++;
+        this.availableSkillPoints += 2;
+
+        // this checks if the exp points surpasses the current exp limit many times it calls the levelUp method again. 
+        if(this.experience >= ExperienceTolevel[this.level]){
+            this.levelUp();
+        }
+    }
+
+    equippedNewArmor(armor: Armor) : void{
+        // this condition will check if the hero has an equipped armor
+        if(this.equippedArmor){
+            // this refreshes the value of defense points once we equip different armors
+            this.barriers.attack -= this.equippedArmor.defensePts;
+        }
+        // this sets the armor we passed from the creation of hero class and setting it as the armor of the character
+        this.equippedArmor = armor;
+        // this adds the current defense points to the attack barrier of the character
+        this.barriers.attack += armor.defensePts;
+    }
+
+    equippedNewWeapon(weapon: Weapon) : void{
+        this.equippedWeapon = weapon;
+    }
+
+    rest(): void {
+        this.currentHealth = this.maxHealth;
+        this.isIncapacitated = false;
+        this.turnsUntilSpecialAvailableAgain = 0;
+    }
+
+}
+
+export class Warrior  extends Hero {
+    constructor(name, gender, race, level, health, skills, weapon, armor){
+        super(name, gender, race, level, health, skills, weapon, armor);
+
+    }
+
+
+}
+export class Archer  extends Hero {
+    constructor(name, gender, race, level, health, skills, weapon, armor){
+        super(name, gender, race, level, health, skills, weapon, armor);
+
+    }
+
+}
+export class Mage  extends Hero {
+    constructor(name, gender, race, level, health, skills, weapon, armor){
+        super(name, gender, race, level, health, skills, weapon, armor);
+
+    }
+
+}
+export class Cleric  extends Hero {
+    constructor(name, gender, race, level, health, skills, weapon, armor){
+        super(name, gender, race, level, health, skills, weapon, armor);
+
     }
 
 }
