cb6577acfb336c2bfddb5f4ad58763a416b25656
gierMann <thisisgie.21@gmail.com>
2020-06-19 15:10:26 +0700

    update: added base structure of fight component in html

--- rpg-app/src/app/components/fight/fight/fight.component.ts
+++ rpg-app/src/app/components/fight/fight/fight.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fight',
+  selector: 'fight-component',
   templateUrl: './fight.component.html',
   styleUrls: ['./fight.component.scss']
 })
