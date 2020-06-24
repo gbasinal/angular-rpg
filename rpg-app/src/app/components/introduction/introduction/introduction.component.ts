a58cef9ee156902959340078e0a7acb7f8c24caf
gierMann <thisisgie.21@gmail.com>
2020-06-24 13:50:22 +0700

    update: added basic animation transition to elements

--- rpg-app/src/app/components/introduction/introduction/introduction.component.ts
+++ rpg-app/src/app/components/introduction/introduction/introduction.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import { gsap } from "gsap";
+
+declare let gsap :any;
 
 @Component({
   selector: 'introduction-component',
@@ -10,8 +13,29 @@ export class IntroductionComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.introductionMessageAnimation(); 
   }
 
+  delay : number = 1500;
+  
+
+  introductionMessageAnimation():void {
+    setTimeout(()=> {
+      gsap.to(".animated-child", {
+        y: 0, opacity: 1,
+        stagger: { 
+          each: .3,
+          from: 0,
+          ease: "ease.inOut"
+        }
+      });
+    },this.delay)
+
+  }
+  
+
+
   introductionMessage : string[] = [];
+
 
 }
